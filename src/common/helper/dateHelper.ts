b51a242d27167aa1f050041283007d954296b8ca
DanielMesa99 <92342596+DanielMesa99@users.noreply.github.com>
2024-12-01 10:09:15 -0500

    feat: add `dateHelper` for date management
    - Formatted code in `ChatItem` component for better understanding and to use `dateHelper`.
    - Moved the date formatting function to a separate helper for better code modularity.

---
+++ src/common/helper/dateHelper.ts
@@ -0,0 +1,24 @@
+/**
+ * Formats the given timestamp to a more readable format.
+ * 
+ * @helper
+ * @example
+ * // Example usage:
+ * const date = formatDate(timestamp);
+ *
+ * @param {string} timestamp - The timestamp to be formatted.
+ * @returns {string} The formatted date or time string.
+ */
+const formatDate = (timestamp: string): string => {
+    const date = new Date(timestamp);
+    const today = new Date();
+
+    if (date.toDateString() === today.toDateString()) {
+        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    } else {
+        return date.toLocaleDateString();
+    }
+};
+
+
+export { formatDate };
